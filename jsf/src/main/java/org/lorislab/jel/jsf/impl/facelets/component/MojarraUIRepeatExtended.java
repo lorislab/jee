8d3a84f1a6744a8201f9e9aa863e03e6a96b8381
Andrej Petras <andrej@ajka-andrej.com>
2015-01-04 08:50:53 +0100

    Update the UIRepeat

--- jsf/src/main/java/org/lorislab/jel/jsf/impl/facelets/component/MojarraUIRepeatExtended.java
+++ jsf/src/main/java/org/lorislab/jel/jsf/impl/facelets/component/MojarraUIRepeatExtended.java
@@ -13,37 +13,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.lorislab.jel.jsf.api.facelets.component;
+package org.lorislab.jel.jsf.impl.facelets.component;
 
 import com.sun.faces.facelets.component.UIRepeat;
 import java.util.Collection;
 import javax.faces.model.CollectionDataModel;
 
 /**
- * The extend UIRepeat faces component.
- * Add support for the collection.
- * 
+ * The extend MOJARRA JS implementation UIRepeat faces component. Add support
+ * for the collection.
+ *
  * @author Andrej_Petras
  */
-public class UIRepeatExtended extends UIRepeat {
+public class MojarraUIRepeatExtended extends UIRepeat {
 
     /**
      * {@inheritDoc }
-     * 
-     * @return if the object is a instance of the {@code java.util.Collection} return {@code javax.faces.model.CollectionDataModel}
-     * 
-     * @see com.sun.faces.facelets.component.UIRepeat#getValue() 
-     * @see com.sun.faces.facelets.component.UIRepeat#getDataModel() 
+     *
+     * @return if the object is a instance of the {@code java.util.Collection}
+     * return {@code javax.faces.model.CollectionDataModel}
+     *
+     * @see com.sun.faces.facelets.component.UIRepeat#getValue()
+     * @see com.sun.faces.facelets.component.UIRepeat#getDataModel()
      */
     @Override
     public Object getValue() {
         Object object = super.getValue();
         if (object != null) {
             if (object instanceof Collection) {
-                object = new CollectionDataModel((Collection)object);
+                object = new CollectionDataModel((Collection) object);
             }
         }
         return object;
     }
-    
+
 }
